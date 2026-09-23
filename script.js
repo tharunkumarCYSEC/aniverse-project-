d226852e44227b8d0f1f11cf17b702f354b2bcba
THARUN <tharun02012009@gmail.com>
2026-09-23 14:25:11 +0530

    Cleaned code and added Spotify and sticky notes features

--- script.js
+++ script.js
@@ -1,206 +1,118 @@
-// --- 1. State & Variables ---
-var biggestIndex = 1;
-var selectedIcon = undefined;
-var topBar = document.querySelector("#top");
+var layerIndex = 10;
 
-// Select Elements
-var welcomeScreen = document.querySelector("#welcome");
-var notesScreen = document.querySelector("#notes");
-var loreScreen = document.querySelector("#lore");
+function openMyWindow(winElement) {
+    winElement.style.display = "flex";
+    layerIndex++;
+    winElement.style.zIndex = layerIndex;
+}
 
-var welcomeClose = document.querySelector("#welcomeclose");
-var notesClose = document.querySelector("#notesclose");
-var loreClose = document.querySelector("#loreclose");
+function closeMyWindow(winElement) {
+    winElement.style.display = "none";
+}
 
-var welcomeOpen = document.querySelector("#welcomeopen");
-var notesAppIcon = document.querySelector("#notesappicon");
-var loreAppIcon = document.querySelector("#loreappicon");
+document.querySelector("#welcomeopen").addEventListener("click", function() {
+    openMyWindow(document.querySelector("#welcome"));
+});
 
-// --- 2. Advanced App Interactive Logic (Quotes App) ---
-var quotesList = [
-  "\"People cannot show each other their true feelings. Fear, suspicion, and resentment never subside.\"",
-  "\"Wake up to reality! Nothing ever goes as planned in this accursed world.\"",
-  "\"The moment people come to know love, they run the risk of carrying hate.\"",
-  "\"I'm about to enter hell... but you'll be joining me.\""
+document.querySelector("#notesappicon").addEventListener("click", function() {
+    openMyWindow(document.querySelector("#notes"));
+});
+
+document.querySelector("#loreappicon").addEventListener("click", function() {
+    openMyWindow(document.querySelector("#lore"));
+});
+
+document.querySelector("#audioappicon").addEventListener("click", function() {
+    openMyWindow(document.querySelector("#audioWindow"));
+});
+
+document.querySelector("#welcomeclose").addEventListener("click", function() {
+    closeMyWindow(document.querySelector("#welcome"));
+});
+document.querySelector("#notesclose").addEventListener("click", function() {
+    closeMyWindow(document.querySelector("#notes"));
+});
+document.querySelector("#loreclose").addEventListener("click", function() {
+    closeMyWindow(document.querySelector("#lore"));
+});
+document.querySelector("#audioclose").addEventListener("click", function() {
+    closeMyWindow(document.querySelector("#audioWindow"));
+});
+
+var myQuotes = [
+    "\"People cannot show each other their true feelings.\"",
+    "\"Wake up to reality! Nothing ever goes as planned.\"",
+    "\"The moment people come to know love, they run the risk of carrying hate.\""
 ];
-var currentQuoteIndex = 0;
+var quoteNum = 0;
 
-var nextQuoteBtn = document.querySelector("#nextQuoteBtn");
-var quoteDisplay = document.querySelector("#quoteDisplay");
-
-nextQuoteBtn.addEventListener("click", function() {
-  currentQuoteIndex = (currentQuoteIndex + 1) % quotesList.length;
-  quoteDisplay.innerHTML = quotesList[currentQuoteIndex];
+document.querySelector("#nextQuoteBtn").addEventListener("click", function() {
+    quoteNum = (quoteNum + 1) % myQuotes.length;
+    document.querySelector("#quoteDisplay").innerHTML = myQuotes[quoteNum];
 });
 
-// --- 3. Dynamic Content Array for Notes App ---
-var content = [
-  {
-    title: "The Reality",
-    date: "06/28/2023",
-    content: `
-      <h3>Wake up to reality...</h3>
-      <p>Nothing ever goes as planned in this accursed world. The longer you live, the more you realize that the only things that truly exist in this reality are merely pain, suffering, and futility.</p>
-    `
-  },
-  {
-    title: "Infinite Tsukuyomi",
-    date: "06/29/2023",
-    content: `
-      <h3>Creating a Dream World</h3>
-      <p>A world where everyone can have their deepest desires fulfilled without experiencing heartbreak, loss, or grief.</p>
-    `
-  }
+var myNotes = [
+    { title: "The Reality", text: "<p>Pain, suffering, and futility are all that exist.</p>" },
+    { title: "Infinite Dream", text: "<p>A world where everyone's desires come true.</p>" }
 ];
 
-// --- 4. Window Management Functions ---
-function openWindow(element) {
-  element.style.display = "flex";
-  biggestIndex++;
-  element.style.zIndex = biggestIndex;
-  topBar.style.zIndex = biggestIndex + 1;
+var sidebarBox = document.querySelector("#sidebar");
+var notesArea = document.querySelector("#notesContentDisplay");
+
+for (let i = 0; i < myNotes.length; i++) {
+    let itemDiv = document.createElement("div");
+    itemDiv.innerHTML = "<b>" + myNotes[i].title + "</b>";
+    itemDiv.addEventListener("click", function() {
+        notesArea.innerHTML = myNotes[i].text;
+    });
+    sidebarBox.appendChild(itemDiv);
 }
+notesArea.innerHTML = myNotes[0].text;
 
-function closeWindow(element) {
-  element.style.display = "none";
+function showLiveTime() {
+    var currentTimeString = new Date().toLocaleTimeString();
+    document.querySelector("#timeElement").innerHTML = currentTimeString;
 }
+setInterval(showLiveTime, 1000);
 
-function handleWindowTap(element) {
-  biggestIndex++;
-  element.style.zIndex = biggestIndex;
-  topBar.style.zIndex = biggestIndex + 1;
-  if (selectedIcon) {
-    deselectIcon(selectedIcon);
-  }
-}
-
-function initializeWindow(element) {
-  element.addEventListener("mousedown", function() {
-    handleWindowTap(element);
-  });
-}
-
-initializeWindow(welcomeScreen);
-initializeWindow(notesScreen);
-initializeWindow(loreScreen);
-
-// --- 5. Event Listeners for Open / Close ---
-welcomeClose.addEventListener("click", () => closeWindow(welcomeScreen));
-notesClose.addEventListener("click", () => closeWindow(notesScreen));
-loreClose.addEventListener("click", () => closeWindow(loreScreen));
-
-welcomeOpen.addEventListener("click", () => openWindow(welcomeScreen));
-
-notesAppIcon.addEventListener("click", function(e) {
-  e.stopPropagation();
-  selectIcon(notesAppIcon);
-  openWindow(notesScreen);
+var isNormalVibe = true;
+document.querySelector("#themeToggleBtn").addEventListener("click", function() {
+    isNormalVibe = !isNormalVibe;
+    if (isNormalVibe) {
+        document.body.style.filter = "none";
+        this.innerHTML = " Change Vibe";
+    } else {
+        document.body.style.filter = "invert(1) hue-rotate(180deg)";
+        this.innerHTML = " Inverted Mode";
+    }
 });
 
-loreAppIcon.addEventListener("click", function(e) {
-  e.stopPropagation();
-  selectIcon(loreAppIcon);
-  openWindow(loreScreen);
-});
-
-// --- 6. Icon Selection Logic ---
-function selectIcon(element) {
-  if (selectedIcon && selectedIcon !== element) {
-    deselectIcon(selectedIcon);
-  }
-  element.classList.add("selected");
-  selectedIcon = element;
-}
-
-function deselectIcon(element) {
-  if (element) {
-    element.classList.remove("selected");
-    selectedIcon = undefined;
-  }
-}
-
-document.body.addEventListener("click", function() {
-  if (selectedIcon) {
-    deselectIcon(selectedIcon);
-  }
-});
-
-// --- 7. Render Notes Dynamic Content ---
-function setNotesContent(index) {
-  var displayArea = document.querySelector("#notesContentDisplay");
-  displayArea.innerHTML = content[index].content;
-}
-
-function addToSideBar(index) {
-  var sidebar = document.querySelector("#sidebar");
-  var note = content[index];
-  var newDiv = document.createElement("div");
-  
-  newDiv.innerHTML = `
-    <p style="margin: 0px; font-weight: 600; color: #fff;">${note.title}</p>
-    <p style="font-size: 11px; margin: 2px 0 0 0; color: #888;">${note.date}</p>
-  `;
-  
-  newDiv.addEventListener("click", function() {
-    setNotesContent(index);
-  });
-  
-  sidebar.appendChild(newDiv);
-}
-
-for (let i = 0; i < content.length; i++) {
-  addToSideBar(i);
-}
-setNotesContent(0);
-
-// --- 8. Draggable Windows Logic ---
-dragElement(welcomeScreen);
-dragElement(notesScreen);
-dragElement(loreScreen);
-
-function dragElement(element) {
-  var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
-
-  if (document.getElementById(element.id + "header")) {
-    document.getElementById(element.id + "header").onmousedown = startDragging;
-  } else {
-    element.onmousedown = startDragging;
-  }
-
-  function startDragging(e) {
-    e = e || window.event;
-    e.preventDefault();
-    initialX = e.clientX;
-    initialY = e.clientY;
-    document.onmouseup = stopDragging;
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    currentX = initialX - e.clientX;
-    currentY = initialY - e.clientY;
-    initialX = e.clientX;
-    initialY = e.clientY;
+document.querySelector("#stickyAppIcon").addEventListener("click", function() {
+    let container = document.querySelector("#stickyContainer");
     
-    element.style.transform = "none";
-    element.style.top = (element.offsetTop - currentY) + "px";
-    element.style.left = (element.offsetLeft - currentX) + "px";
-  }
-
-  function stopDragging() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-// --- 9. Live Clock Logic ---
-function updateTime() {
-    var currentTime = new Date().toLocaleString();
-    var timeText = document.querySelector("#timeElement");
-    timeText.innerHTML = currentTime;
-}
-
-updateTime();
-setInterval(updateTime, 1000);
+    let noteDiv = document.createElement("div");
+    noteDiv.className = "sticky-note";
+    
+    let noteTitle = document.createElement("b");
+    noteTitle.innerText = "Sticky Note:";
+    
+    let noteInput = document.createElement("textarea");
+    noteInput.placeholder = "Type your note here...";
+    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.style.float = "right";
+    deleteBtn.style.background = "red";
+    deleteBtn.style.color = "white";
+    deleteBtn.style.border = "none";
+    deleteBtn.style.cursor = "pointer";
+    
+    deleteBtn.addEventListener("click", function() {
+        container.removeChild(noteDiv);
+    });
+    
+    noteDiv.appendChild(deleteBtn);
+    noteDiv.appendChild(noteTitle);
+    noteDiv.appendChild(noteInput);
+    container.appendChild(noteDiv);
+});
